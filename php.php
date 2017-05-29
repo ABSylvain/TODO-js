70cc4845b535912432a9807918e53cb7ca842fc4
BobRockeur <arnaudbouriquesylvain@gmail.com>
2017-05-29 14:58:05 +0200

    Big Commit

---
+++ php.php
@@ -0,0 +1,16 @@
+bloup
+<?php
+phpinfo();
+    /*declarer une variable*/
+    $myVar = 'bloop';
+    /*un echo*/
+    echo 'un truc <br/>';
+    /*echo d'une function*/
+    echo mafonction ("bloop");
+    /*connaitre les options de php (uniquement pour dev)*/
+    
+    /*Une function*/
+    function myfunct ($argument){
+        return $argument . 'kekchose';
+    };
+?>
